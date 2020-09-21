4aab8d0c35d3aadb59bcd6acb3666192e8efcdc1
Sagun Singh <sagun.singh.vt@gmail.com>
2020-09-21 11:02:11 -0400

    Add HasDefault instances for prelude types

--- src/Flat/Transient.hs
+++ src/Flat/Transient.hs
@@ -12,3 +12,11 @@ instance (HasDefault a) => Flat.Flat (Transient a) where
     encode _ = encode ()
     decode   = (Transient defaultValue) <$ (decode :: Get ())
     size   _ = size ()
+
+instance HasDefault [a]       where defaultValue = []
+instance HasDefault (Maybe a) where defaultValue = Nothing
+instance HasDefault Int       where defaultValue = 0
+instance HasDefault Integer   where defaultValue = 0
+instance HasDefault Float     where defaultValue = 0.0
+instance HasDefault Double    where defaultValue = 0.0
+instance HasDefault Char      where defaultValue = '\0'
