8791f83e64d6fdeb52af97af91d8ed19006faf5a
Sagun Singh <sagun.singh.vt@gmail.com>
2020-09-21 11:07:36 -0400

    Replace default values with errors
    Remove the HasDefault class. Rewrite the Flat instance for the
    Transient wrapper to return an error notifying the user that the
    transient value was never initialized. This change takes advantage of
    Haskell's lazy evaluation to simplify implementation of transient
    types.

--- src/Flat/Transient.hs
+++ src/Flat/Transient.hs
@@ -5,18 +5,7 @@ import Flat.Decoder.Types
 
 data Transient a = Transient a
 
-class HasDefault a where
-    defaultValue :: a
-
-instance (HasDefault a) => Flat.Flat (Transient a) where
+instance Flat.Flat (Transient a) where
     encode _ = encode ()
-    decode   = (Transient defaultValue) <$ (decode :: Get ())
+    decode   = (Transient $ error "Transient value was never initialized.") <$ (decode :: Get ())
     size   _ = size ()
-
-instance HasDefault [a]       where defaultValue = []
-instance HasDefault (Maybe a) where defaultValue = Nothing
-instance HasDefault Int       where defaultValue = 0
-instance HasDefault Integer   where defaultValue = 0
-instance HasDefault Float     where defaultValue = 0.0
-instance HasDefault Double    where defaultValue = 0.0
-instance HasDefault Char      where defaultValue = '\0'
